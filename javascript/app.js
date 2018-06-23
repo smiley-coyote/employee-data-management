1533c5fab70aefd8006b8cd28ef3055e49b8fe60
smiley-coyote <nick.a.alex@gmail.com>
2018-06-23 10:50:38 -0400

    worked on javascript

--- javascript/app.js
+++ javascript/app.js
@@ -18,10 +18,24 @@ $("#add-user").on("click", function (event) {
         name: $('#name-input').val().trim(),
         role: $('#role-input').val().trim(),
         date: $('#date-input').val().trim(),
-        rate: $('#rate-input').val().trim()
+        rate: $('#rate-input').val().trim(),
+        // dateAdded: database.ServerValue.TIMESTAMP
 
     })
 
     $('#theForm')[0].reset();
 
+})
+database.ref().on("child_added", function(snapshot) {
+    var tableRow = $("<tr>");
+    var tableData = $("<td>");
+    tableData.text(snapshot.val().name);
+    tableRow.append(tableData);
+    tableData.text(snapshot.val().role);
+    tableRow.append(tableData);
+    tableData.text(snapshot.val().date);
+    tableRow.append(tableData);
+    tableData.text(snapshot.val().rate);
+    tableRow.append(tableData);
+    $("#table-body").append(tableRow);
 })
